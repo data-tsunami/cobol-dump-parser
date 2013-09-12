6697f3f4bc58554d49c02cd2320548f25c96636e
Horacio G. de Oro <hgdeoro@gmail.com>
2013-09-12 14:39:03 -0300

    Do faster preformance test by default

--- src/test/java/ar/com/datatsunami/bigdata/cobol/ParseDataSetTest.java
+++ src/test/java/ar/com/datatsunami/bigdata/cobol/ParseDataSetTest.java
@@ -119,6 +119,13 @@ public class ParseDataSetTest {
 
 	@Test
 	public void comparePerformance() throws ParserException {
+
+		long iterations;
+		if (System.getenv().containsKey("ITERATIONS"))
+			iterations = Long.parseLong(System.getenv("ITERATIONS"));
+		else
+			iterations = 100000;
+
 		/*
 		 * Default
 		 */
@@ -126,7 +133,7 @@ public class ParseDataSetTest {
 		this.addFieldsToCobolDumpParser(cdpDefault);
 
 		final long startDefault = System.currentTimeMillis();
-		for (int i = 0; i < 1000000; i++)
+		for (int i = 0; i < iterations; i++)
 			cdpDefault.getItemsWithLabels(line1).get("Code");
 		final long endDefault = System.currentTimeMillis();
 
@@ -138,7 +145,7 @@ public class ParseDataSetTest {
 
 		final long startPositional = System.currentTimeMillis();
 		String[] fieldsNamed = new String[] { "Code" };
-		for (int i = 0; i < 1000000; i++)
+		for (int i = 0; i < iterations; i++)
 			cdpDefault.getItemsValues(line1, fieldsNamed);
 		final long endPositional = System.currentTimeMillis();
 
