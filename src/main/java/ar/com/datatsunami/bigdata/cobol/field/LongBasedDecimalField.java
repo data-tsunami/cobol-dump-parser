ff15337426f21579538afe677f747c1583deb41a
Horacio G. de Oro <hgdeoro@gmail.com>
2013-09-15 21:29:24 -0300

    Add LongBasedDecimalFormat

---
+++ src/main/java/ar/com/datatsunami/bigdata/cobol/field/LongBasedDecimalField.java
@@ -0,0 +1,15 @@
+package ar.com.datatsunami.bigdata.cobol.field;
+
+import ar.com.datatsunami.bigdata.cobol.format.LongBasedDecimalFormat;
+
+public class LongBasedDecimalField extends Field<Long> {
+
+	public LongBasedDecimalField(int cantidadLugares, String label, int decimalPlaces) {
+		super(cantidadLugares, label, new LongBasedDecimalFormat(decimalPlaces));
+	}
+
+	public LongBasedDecimalField(int cantidadLugares, String label, int decimalPlaces, boolean withSign) {
+		super(cantidadLugares, label, new LongBasedDecimalFormat(decimalPlaces, withSign));
+	}
+
+}
