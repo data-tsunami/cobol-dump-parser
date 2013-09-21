72e1816e8c0b143aa256d9c30b169bebc064e461
Horacio G. de Oro <hgdeoro@gmail.com>
2013-09-21 15:56:27 -0300

    Add FixedWidthLoaderByStaticFunc

---
+++ src/main/java/ar/com/datatsunami/pig/FixedWidthLoaderByStaticFunc.java
@@ -0,0 +1,79 @@
+package ar.com.datatsunami.pig;
+
+import ar.com.datatsunami.bigdata.cobol.CobolDumpParser;
+import ar.com.datatsunami.bigdata.cobol.linehandler.LineHandler;
+import ar.com.datatsunami.bigdata.cobol.linehandler.PositionalLineHandler;
+
+public class FixedWidthLoaderByStaticFunc extends FixedWidthLoader {
+
+	/**
+	 * UDF for Pig to load data and to reuse the format from a CobolDumpParser
+	 * instance.
+	 * 
+	 * This UDF doesn't support all the datatypes that CobolDumpParser supports.
+	 * 
+	 * @param staticFunction
+	 * @param fields
+	 */
+	public FixedWidthLoaderByStaticFunc(String staticFunction, String fields) {
+		this(get(staticFunction, fields));
+	}
+
+	/**
+	 * Instantiates CobolDumpParser
+	 * 
+	 * @param staticFunction
+	 *            Reference to static functions that creates the CobolDumpParser
+	 *            instance
+	 * @return
+	 */
+	public static String[] get(String staticFunction, String fields) {
+
+		/*
+		 * Parse field indexes
+		 */
+		String fieldTokens[] = fields.split(",");
+		int fieldIndexes[] = new int[fieldTokens.length];
+		for (int i = 0; i < fieldTokens.length; i++)
+			fieldIndexes[i] = Integer.valueOf(fieldTokens[i]);
+
+		/*
+		 * Call static method
+		 */
+		Object ret = StaticMethodCaller.call(staticFunction);
+
+		if (ret == null)
+			throw new RuntimeException("The method referenced by '" + staticFunction
+					+ "' didn't returned anything");
+
+		if (!CobolDumpParser.class.isAssignableFrom(ret.getClass())) {
+			throw new RuntimeException("The method referenced by '" + staticFunction
+					+ "' returned an instance of typ '" + ret.getClass().getCanonicalName()
+					+ "', which is not a CobolDumpParser instance.");
+		}
+
+		/*
+		 * Check received object & validate CobolDumpParser
+		 */
+
+		CobolDumpParser cobolDumpParser = (CobolDumpParser) ret;
+		LineHandler lineHandler = cobolDumpParser.getLineHandler();
+
+		if (!PositionalLineHandler.class.isAssignableFrom(lineHandler.getClass())) {
+			throw new RuntimeException("The lineHandler if of type '"
+					+ lineHandler.getClass().getCanonicalName() + "', which is not a PositionalLineHandler.");
+		}
+
+		PositionalLineHandler positionalLineHandler = (PositionalLineHandler) lineHandler;
+
+		return positionalLineHandler.getFixedWidthLoaderSpec(fieldIndexes);
+	}
+
+	/**
+	 * DON'T USE THIS! Used internally.
+	 */
+	protected FixedWidthLoaderByStaticFunc(String[] columnSpecAndSchemaStr) {
+		super(columnSpecAndSchemaStr[0], "", columnSpecAndSchemaStr[1]);
+	}
+
+}
---
+++ src/test/java/ar/com/datatsunami/pig/FixedWidthLoaderByStaticFuncTest.java
@@ -0,0 +1,50 @@
+package ar.com.datatsunami.pig;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import ar.com.datatsunami.bigdata.cobol.CobolDumpParser;
+import ar.com.datatsunami.bigdata.cobol.CobolDumpParserForHadoop;
+import ar.com.datatsunami.bigdata.cobol.linehandler.LineHandlerTestUtils;
+import ar.com.datatsunami.bigdata.cobol.linehandler.PositionalLineHandler;
+
+public class FixedWidthLoaderByStaticFuncTest {
+
+	public static CobolDumpParser cobolDumpParserFactory() {
+		final CobolDumpParser cdp = new CobolDumpParser(new PositionalLineHandler());
+		LineHandlerTestUtils.addFieldsToCobolDumpParser(cdp);
+		return cdp;
+	}
+
+	public static CobolDumpParser cobolDumpParserFactoryForHadoop() {
+		final CobolDumpParser cdp = new CobolDumpParserForHadoop(new PositionalLineHandler());
+		LineHandlerTestUtils.addFieldsToCobolDumpParser(cdp);
+		return cdp;
+	}
+
+	// cp.add(new LongField(6, "ItemID"));
+	// cp.add(new StringField(5, "Code"));
+	// cp.add(new StringField(15, "Description"));
+	// cp.add(new DecimalField(8, "Price", 2, true));
+	// cp.add(new DecimalField(6, "Index", 3, false));
+	// cp.add(new LongBasedDecimalField(4, "Value", 1, false));
+
+	@Test
+	public void testFixedWidthLoaderByStaticFunc() {
+		String columnSpecAndSchemaStr[] = FixedWidthLoaderByStaticFunc.get(
+				"ar.com.datatsunami.pig.FixedWidthLoaderByStaticFuncTest.cobolDumpParserFactory", "0");
+		assertEquals("1-6", columnSpecAndSchemaStr[0]);
+		assertEquals("itemid:long", columnSpecAndSchemaStr[1]);
+	}
+
+	@Test
+	public void testFixedWidthLoaderByStaticFuncCDPForHadoop() {
+		String columnSpecAndSchemaStr[] = FixedWidthLoaderByStaticFunc.get(
+				"ar.com.datatsunami.pig.FixedWidthLoaderByStaticFuncTest.cobolDumpParserFactoryForHadoop",
+				"1");
+		assertEquals("7-11", columnSpecAndSchemaStr[0]);
+		assertEquals("code:chararray", columnSpecAndSchemaStr[1]);
+	}
+
+}
