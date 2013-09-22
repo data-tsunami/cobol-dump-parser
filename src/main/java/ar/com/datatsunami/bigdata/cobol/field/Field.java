19f3951600a613009b355f63ec345806dd998e06
Horacio G. de Oro <hgdeoro@gmail.com>
2013-09-22 11:01:38 -0300

    Add javadoc to getPigSchema()

--- src/main/java/ar/com/datatsunami/bigdata/cobol/field/Field.java
+++ src/main/java/ar/com/datatsunami/bigdata/cobol/field/Field.java
@@ -36,6 +36,20 @@ public abstract class Field<I> {
 	 */
 	protected long errorCount = 0;
 
+	/**
+	 * Returns a schema object used to map the field to Pig field (or fields).
+	 * 
+	 * A field here can result in many fields of Pig, for example:
+	 * <code>FixedWidthLoader</code>, which is expresed in at least 2 Pig
+	 * fields: one for the integer part, other for the decimal part (and,
+	 * optionally, one thirth Pig field for the sign).
+	 * 
+	 * This is a workaround to avoid implementing a custom UDF, and use
+	 * <code>FixedWidthLoader</code>. This method returns a PigSchema instance,
+	 * used to configure <code>FixedWidthLoader</code>.
+	 * 
+	 * @return
+	 */
 	public abstract PigSchema getPigSchema();
 
 	public Field(int cantidadLugares, String label, Format<I> format) {
